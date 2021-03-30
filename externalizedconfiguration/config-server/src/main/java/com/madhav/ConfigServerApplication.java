af001dfe065696f222ef478acaaece95382aa8da
madhav123 <gmrdizzy@gmail.com>
2021-03-30 13:56:08 +0530

    Externalized configuration

---
+++ externalizedconfiguration/config-server/src/main/java/com/madhav/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.madhav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.util.StringUtils;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
