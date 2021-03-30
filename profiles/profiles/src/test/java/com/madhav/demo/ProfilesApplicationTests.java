8e1ea24225a8cfb8892cc285d928671b4022188c
madhav123 <gmrdizzy@gmail.com>
2021-03-30 14:00:33 +0530

    Profiles

---
+++ profiles/profiles/src/test/java/com/madhav/demo/ProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhav.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
